5412d9c16eec49463bb0523d3f84084cdb3c01b6
jonayeddewan <2010006@student.ruet.ac.bd>
2024-07-01 18:20:37 +0600

    change some style of login page of student portal. modify some code in find-result.php, create dashboard for student portal but need to work more for style of the dashboard of studentportal, i have modified the course registration page but need to work more in the page.

--- srms/find-result.php
+++ srms/find-result.php
@@ -1,110 +1,118 @@
 <?php
 session_start();
-//error_reporting(0);
-include('includes/config.php');?>
+include('includes/config.php');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $rollid = $_POST['rollid'];
+    $class = $_POST['class'];
+
+    $sql = "SELECT * FROM tblstudents WHERE RollId=:rollid AND Department=:class";
+    $query = $dbh->prepare($sql);
+    $query->bindParam(':rollid', $rollid, PDO::PARAM_STR);
+    $query->bindParam(':class', $class, PDO::PARAM_STR);
+    $query->execute();
+    $result = $query->fetch(PDO::FETCH_OBJ);
+
+    if ($result) {
+        $_SESSION['login'] = $result->RollId;
+        header("Location: student-dash.php");
+        exit();
+    } else {
+        $error = "Invalid Roll ID or Department";
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    	<meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>School Result Management System</title>
-        <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" >
-        <link rel="stylesheet" href="css/font-awesome.min.css" media="screen" >
-        <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen" >
-        <link rel="stylesheet" href="css/icheck/skins/flat/blue.css" >
-        <link rel="stylesheet" href="css/main.css" media="screen" >
-        <script src="js/modernizr/modernizr.min.js"></script>
-    </head>
-    <body class="">
-        <div class="main-wrapper">
 
-            <div class="login-bg-color bg-black-300">
-                <div class="row">
-                    <div class="col-md-4 col-md-offset-4">
-                        <div class="panel login-box">
-                            <div class="panel-heading">
-                                <div class="panel-title text-center">
-                                    <h4>School Result Management System</h4>
-                                </div>
-                            </div>
-                            <div class="panel-body p-20">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>School Result Management System</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" media="screen">
+    <link rel="stylesheet" href="css/font-awesome.min.css" media="screen">
+    <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen">
+    <link rel="stylesheet" href="css/icheck/skins/flat/blue.css">
+    <link rel="stylesheet" href="css/main.css" media="screen">
+    <script src="js/modernizr/modernizr.min.js"></script>
+</head>
 
-                           
-
-                                <form action="result.php" method="post">
-                                	<div class="form-group">
-                                		<label for="rollid">Enter your Roll Id</label>
-                                        <input type="text" class="form-control" id="rollid" placeholder="Enter Your Roll Id" autocomplete="off" name="rollid">
-                                	</div>
-                               <div class="form-group">
-                                                        <label for="default" class="col-sm-2 control-label">Class</label>
- <select name="class" class="form-control" id="default" required="required">
-<option value="">Select Class</option>
-<?php $sql = "SELECT * from tblclasses";
-$query = $dbh->prepare($sql);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-if($query->rowCount() > 0)
-{
-foreach($results as $result)
-{   ?>
-<option value="<?php echo htmlentities($result->id); ?>"><?php echo htmlentities($result->ClassName); ?>&nbsp; Section-<?php echo htmlentities($result->Section); ?></option>
-<?php }} ?>
- </select>
-</div>
-
-    
-                                    <div class="form-group mt-20">
-                                        <div class="">
-                                      
-                                            <button type="submit" class="btn btn-success btn-labeled pull-right">Search<span class="btn-label btn-label-right"><i class="fa fa-check"></i></span></button>
-                                            <div class="clearfix"></div>
-                                        </div>
-                                    </div>
-
-                                       <div class="col-sm-6">
-                                                               <a href="index.php">Back to Home</a>
-                                                            </div>
-                                </form>
-
-                                <hr>
-
+<body class="">
+    <div class="main-wrapper">
+        <div class="login-bg-color bg-black-300">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                    <div class="panel login-box">
+                        <div class="panel-heading">
+                            <div class="panel-title text-center">
+                                <h4>School Result Management System</h4>
                             </div>
                         </div>
-                        <!-- /.panel -->
-                        <p class="text-muted text-center"><small>Student Result Management System</small></p>
+                        <div class="panel-body p-20">
+                            <form action="" method="post">
+                                <div class="form-group">
+                                    <label for="rollid">Enter your Roll Id</label>
+                                    <input type="text" class="form-control" id="rollid" placeholder="Enter Your Roll Id" autocomplete="off" name="rollid" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="default" class="col-sm-2 control-label">Department</label>
+                                    <select name="class" class="form-control" id="default" required="required">
+                                        <option value="">Select Department</option>
+                                        <?php 
+                                        $sql = "SELECT DISTINCT ClassName from tblclasses";
+                                        $query = $dbh->prepare($sql);
+                                        $query->execute();
+                                        $results=$query->fetchAll(PDO::FETCH_OBJ);
+                                        if($query->rowCount() > 0)
+                                        {
+                                            foreach($results as $result)
+                                            {   
+                                                ?>
+                                                <option value="<?php echo htmlentities($result->ClassName); ?>">
+                                                    <?php echo htmlentities($result->ClassName); ?>
+                                                </option>
+                                            <?php 
+                                            }
+                                        } 
+                                        ?>
+                                    </select>
+                                </div>
+                                <?php if(isset($error)) { ?>
+                                    <div class="alert alert-danger">
+                                        <?php echo $error; ?>
+                                    </div>
+                                <?php } ?>
+                                <div class="form-group mt-20">
+                                    <button type="submit" class="btn btn-success btn-labeled pull-right">Search<span class="btn-label btn-label-right"><i class="fa fa-check"></i></span></button>
+                                    <div class="col-sm-6">
+                                        <a href="index.php">Back to Home</a>
+                                    </div>
+                                </div>
+                            </form>
+                            <hr>
+                        </div>
                     </div>
-                    <!-- /.col-md-6 col-md-offset-3 -->
+                    <p class="text-muted text-center"><small>Student Result Management System</small></p>
                 </div>
-                <!-- /.row -->
             </div>
-            <!-- /. -->
-
         </div>
-        <!-- /.main-wrapper -->
-
-        <!-- ========== COMMON JS FILES ========== -->
-        <script src="js/jquery/jquery-2.2.4.min.js"></script>
-        <script src="js/jquery-ui/jquery-ui.min.js"></script>
-        <script src="js/bootstrap/bootstrap.min.js"></script>
-        <script src="js/pace/pace.min.js"></script>
-        <script src="js/lobipanel/lobipanel.min.js"></script>
-        <script src="js/iscroll/iscroll.js"></script>
-
-        <!-- ========== PAGE JS FILES ========== -->
-        <script src="js/icheck/icheck.min.js"></script>
-
-        <!-- ========== THEME JS ========== -->
-        <script src="js/main.js"></script>
-        <script>
-            $(function(){
-                $('input.flat-blue-style').iCheck({
-                    checkboxClass: 'icheckbox_flat-blue'
-                });
+    </div>
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <script src="js/jquery-ui/jquery-ui.min.js"></script>
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <script src="js/pace/pace.min.js"></script>
+    <script src="js/lobipanel/lobipanel.min.js"></script>
+    <script src="js/iscroll/iscroll.js"></script>
+    <script src="js/icheck/icheck.min.js"></script>
+    <script src="js/main.js"></script>
+    <script>
+        $(function() {
+            $('input.flat-blue-style').iCheck({
+                checkboxClass: 'icheckbox_flat-blue'
             });
-        </script>
+        });
+    </script>
+</body>
 
-        <!-- ========== ADD custom.js FILE BELOW WITH YOUR CHANGES ========== -->
-    </body>
 </html>
---
+++ srms/student-dash.php
@@ -0,0 +1,104 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+
+if (!isset($_SESSION['login'])) {
+    header("Location: index.php");
+    exit();
+} else {
+    $rollId = $_SESSION['login'];
+    $sql = "SELECT * FROM tblstudents WHERE RollId=:rollId";
+    $query = $dbh->prepare($sql);
+    $query->bindParam(':rollId', $rollId, PDO::PARAM_STR);
+    $query->execute();
+    $result = $query->fetch(PDO::FETCH_OBJ);
+
+    if (!$result) {
+        header("Location: index.php");
+        exit();
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Student Portal | Dashboard</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" media="screen">
+    <link rel="stylesheet" href="css/font-awesome.min.css" media="screen">
+    <link rel="stylesheet" href="css/main.css" media="screen">
+    <script src="js/modernizr/modernizr.min.js"></script>
+</head>
+<body class="top-navbar-fixed">
+    <div class="main-wrapper">
+        <nav class="navbar top-navbar bg-white box-shadow">
+            <div class="container-fluid">
+                <div class="navbar-header no-padding">
+                    <a class="navbar-brand" href="dashboard.php">Student Portal</a>
+                </div>
+                <ul class="nav navbar-nav navbar-right" data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">
+                    <li class="hidden-xs">
+                        <a href="#">Welcome, <?php echo htmlentities($result->StudentName); ?></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="content-wrapper">
+            <div class="content-container">
+                <div class="left-sidebar bg-black-300 box-shadow">
+                    <div class="sidebar-content">
+                        <div class="user-info closed">
+                            <span class="name"><?php echo htmlentities($result->StudentName); ?></span>
+                        </div>
+                        <ul class="list-unstyled">
+                            <li><a href="course-registration.php"><i class="fa fa-pencil"></i> Course Registration</a></li>
+                            <li><a href="result.php"><i class="fa fa-file-text"></i> Check Result</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="main-page">
+                    <div class="container-fluid">
+                        <div class="row page-title-div">
+                            <div class="col-sm-6">
+                                <h2 class="title">Dashboard</h2>
+                            </div>
+                        </div>
+                    </div>
+
+                    <section class="section">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                    <div class="panel">
+                                        <div class="panel-body">
+                                            <h3>Student Information</h3>
+                                            <p><strong>Roll ID:</strong> <?php echo htmlentities($result->RollId); ?></p>
+                                            <p><strong>Class:</strong> <?php echo htmlentities($result->ClassName); ?></p>
+                                            <p><strong>Name:</strong> <?php echo htmlentities($result->StudentName); ?></p>
+                                            <p><strong>Email:</strong> <?php echo htmlentities($result->StudentEmail); ?></p>
+                                            <p><strong>Department:</strong> <?php echo htmlentities($result->Department); ?></p>
+                                            <p><strong>Section:</strong> <?php echo htmlentities($result->Section); ?></p>
+                                            <p><strong>Series:</strong> <?php echo htmlentities($result->Series); ?></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
